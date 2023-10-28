3de16fba151ae39abf02ccb096c58c66fe59116e
shivthedev <citysiva180@users.noreply.github.com>
2023-10-28 17:38:37 +0530

    Problem Solving | Added Comments to Binary

--- binaryarray_sort.py
+++ binaryarray_sort.py
@@ -1,7 +1,12 @@
-a = [0,0,0,1,0,1,0,1,1,1,0,0,0,1,0,1]
-j=-1
+# Problem : Given a binary array, sort it in linear time and constant space.
+# The output should print all zeros, followed by all ones.
+
+
+a = [0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1]
+j = -1
 for i in range(len(a)):
-    if a[i]< 1:
-        j = j+1
-        a[i],a[j] = a[j],a[i]
+    if a[i] < 1:
+        j = j+1  # Index increments only when the value is zero
+        a[i], a[j] = a[j], a[i]  # associative swapping in python
 print(a)
+
